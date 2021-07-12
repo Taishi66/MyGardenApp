52eaab457cc20db073a657600b3619725071f428
Taishi <jctlamy@gmail.com>
2021-07-12 09:28:29 +0200

    git init

---
+++ app/src/main/java/fr/oogiboogi66/mygarden/MainActivity.kt
@@ -0,0 +1,18 @@
+package fr.oogiboogi66.mygarden
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.oogiboogi66.mygarden.fragments.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //injecter le fragment dans notre boite (fragment_container
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+}
